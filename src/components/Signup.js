f61a11f618e9e0e152ea65753afbe9bdb9545d63
Andry Malala <razakanaivoandrymalala@gmail.com>
2024-05-04 19:52:02 +0300

    test

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,6 +6,13 @@ export default function Signup({ handleGoogleSignIn }) {
   return (
     <>
       <div className='containers'>
+     
+        <div className='main-publish'>
+          <h2>Discover the Magic of Little Lemon</h2>
+          <p>Come explore our unique and delicious menu. Each dish is crafted to make you discover the flavors of the world.</p>
+          <p>Don't miss the chance to experience an unforgettable culinary adventure at Little Lemon.</p>
+        </div>
+
         <div className='main-signup'>
           <h2>Welcome the Litle lemmn </h2>
           <b>Would you like to connect with: </b>
@@ -15,11 +22,6 @@ export default function Signup({ handleGoogleSignIn }) {
             type="dark"
             onClick={handleGoogleSignIn}
           />
-        </div>
-        <div className='main-publish'>
-          <h2>popota</h2>
-          <p>Come explore our unique and delicious menu. Each dish is crafted to make you discover the flavors of the world.</p>
-          <p>Don't miss the chance to experience an unforgettable culinary adventure at Little Lemon.</p>
         </div>
       </div>
 
