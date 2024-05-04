d341e068e687394e6b55d4dc89fd5133daf75592
Andry Malala <razakanaivoandrymalala@gmail.com>
2024-05-04 23:08:57 +0300

    update ui page signup

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -83,7 +83,7 @@ export default function HomePage() {
                 <>
                     <Header />
                     <Signup handleGoogleSignIn={handleGoogleSignIn}/>
-                    <Footer />
+                
                 </>
             )}
     
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,19 +1,19 @@
 import './App.css';
-import hotels from '../image/hotels.avif';
+import chefsign from '../image/chef-sign.png';
 import GoogleButton from 'react-google-button';
 
 export default function Signup({ handleGoogleSignIn }) {
   return (
     <>
-      <div className='containers'>
+      <section className='container-sign'>
      
-        <div className='main-publish'>
+        <div className='main-sign1'>
           <h2>Discover the Magic of Little Lemon</h2>
           <p>Come explore our unique and delicious menu. Each dish is crafted to make you discover the flavors of the world.</p>
           <p>Don't miss the chance to experience an unforgettable culinary adventure at Little Lemon.</p>
         </div>
 
-        <div className='main-signup'>
+        <div className='main-sign2'>
           <h2>Welcome the Litle lemmn </h2>
           <b>Would you like to connect with: </b>
 
@@ -23,23 +23,18 @@ export default function Signup({ handleGoogleSignIn }) {
             onClick={handleGoogleSignIn}
           />
         </div>
-      </div>
-
-      <section className='qualites'>
-        <article className='galerie'>
-          <img src={hotels} alt="hotels" />
-        </article>
-
-        <article id='article-qualites'>
-          <h3>Qualities of their work:</h3>
-          <ul>
-            <li>Culinary creativity</li>
-            <li>Mastery of cooking techniques</li>
-            <li>Neat presentation of dishes</li>
-            <li>Consistency in dish quality</li>
-          </ul>
-        </article>
       </section>
+     
+
+      <section>
+      <img src={chefsign} alt="chef brillant" />
+
+  
+
+
+      </section>
+
+   
     </>
   );
 }
